3014a1c721782c251a63bd3236aca32c40414355
priyagund <priyankagund18@gmail.com>
2020-02-09 16:26:25 +0530

    ADDED:Application for power function basckend and front end

---
+++ src/main/java/com/example/dto/DataDTO.java
@@ -0,0 +1,23 @@
+package com.example.dto;
+
+public class DataDTO {
+private int base;
+private int exponent;
+public int getBase() {
+	return base;
+}
+public void setBase(int base) {
+	this.base = base;
+}
+public int getExponent() {
+	return exponent;
+}
+public void setExponent(int exponent) {
+	this.exponent = exponent;
+}
+
+
+
+
+
+}
